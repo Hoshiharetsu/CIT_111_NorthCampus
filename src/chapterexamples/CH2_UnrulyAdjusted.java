f1135b942cead35d0188c7899e09f1f150562c52
Rebecca DuPont <RElinich@gmail.com>
2021-07-22 11:16:14 -0400

    Initial Commit.

---
+++ src/chapterexamples/CH2_UnrulyAdjusted.java
@@ -0,0 +1,14 @@
+
+package chapterexamples;
+/*
+\n is the new line escape sequence
+\t is another sequence, this is for the tab escape, you can try adding it to the code below (pg. 37)
+*/
+
+public class CH2_UnrulyAdjusted {
+    public static void main(String[] args){
+        System.out.print("These are our top sellers: \n");
+        System.out.print("\tComputer Games,\n \tCoffee,\n");
+        System.out.println("\tAsprin");
+    }
+}
